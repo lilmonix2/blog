eeae43d49327be3b4f900db9bc35574456cccd6a
lilmonix2 <xiewenjineagle@gmail.com>
2025-04-18 19:12:39 +0800

    fix: correct file type for logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="flex items-center justify-between">
           <div className="mr-3">
             <Image
-              src="/static/images/logo.webp"
+              src="/static/images/logo.png"
               alt="logo"
               className="h-auto w-auto"
               width={50}
